ebc804504f514f468bebb816080864831760b579
Alex Zafra <azafrom731@g.educaand.es>
2021-09-30 12:56:43 +0200

    Texto cambiado

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Pulsa el botón para incrementar el contador:',
+              'Pulsa el botón + para incrementar el contador:',
             ),
             Text(
               '$_counter',
